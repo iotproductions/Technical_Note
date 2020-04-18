41c73eea6dfd26e0e8bc238468bdcfd71fc63dcf
IOT_Developer <lethanhtrieuk36@gmail.com>
2020-04-18 19:16:51 +0700

    cordova-plugin-compat included in cordova-android 6.3.0.

--- 200417_Cordova_Build_Simple_BLE_App.h
+++ 200417_Cordova_Build_Simple_BLE_App.h
@@ -5,8 +5,14 @@ cd SoliditekBLE
 // Install plugins
 cordova plugin add cordova-plugin-ble-central
 cordova platform add cordova-plugin-compat
+/*
+https://github.com/apache/cordova-plugin-compat
+This plugin is no longer being worked on as the functionality provided by this plugin is now included in cordova-android 6.3.0. 
+You should upgrade your application to use version 1.2.0 of this plugin. 
+It will detect whether or not the plugin is required based on the version of cordova-android your app uses.
+*/
 // Add platform
-cordova platform add android@6.2.0
+cordova platform add android@6.3.0
 // Download and replace www folder
 http://www.soliditek.com/wp-content/uploads/2016/12/SoliditekBLE.zip
 // Build app and run
