b2d1b2846aae620b5a12bd258fe423c086d57565
IOT_Developer <lethanhtrieuk36@gmail.com>
2020-06-20 17:33:11 +0700

    Update 200620_Odroid_Command_App_Notes.h

--- 200620_Odroid_Command_App_Notes.h
+++ 200620_Odroid_Command_App_Notes.h
@@ -33,28 +33,14 @@ node -v
 npm -v
 //----------------------------------------------------------------------------------------------
 // Install Mosquitto MQTT
-// https://www.digitalocean.com/community/questions/how-to-setup-a-mosquitto-mqtt-server-and-receive-data-from-owntracks
+// https://www.vultr.com/docs/how-to-install-mosquitto-mqtt-broker-server-on-ubuntu-16-04
 // Mosquitto is not starting on boot  => https://github.com/eclipse/mosquitto/issues/310
 //----------------------------------------------------------------------------------------------
-sudo adduser mosquitto
-
-sudo apt-get update
-sudo apt-get install build-essential libwrap0-dev libssl-dev libc-ares-dev uuid-dev xsltproc
-sudo wget http://mosquitto.org/files/source/mosquitto-1.5.tar.gz
-sudo tar xvzf mosquitto-1.5.tar.gz
-cd mosquitto-1.5
-sudo make
-sudo make install
+sudo apt-get install mosquitto
 
 sudo mosquitto_passwd -c /etc/mosquitto/pwfile trieu.le
 
-sudo mkdir /var/lib/mosquitto/
-
-sudo chown odroid:odroid /var/lib/mosquitto/ -R
-
-sudo cp /etc/mosquitto/mosquitto.conf.example /etc/mosquitto/mosquitto.conf
-
-sudo nano /etc/mosquitto/mosquitto.conf
+sudo nano /etc/mosquitto/conf.d/default.conf
 
 listener 1883 localhost
 persistence true
@@ -74,8 +60,10 @@ password_file /etc/mosquitto/pwfile
 
 sudo /sbin/ldconfig
 
+sudo systemctl restart mosquitto
+
 // RUN/TEST Broker
-sudo mosquitto -c /etc/mosquitto/mosquitto.conf
+sudo mosquitto -c /etc/mosquitto/conf.d/default.conf
 sudo nano /etc/init/mosquitto.conf
 
 // Type script below:
@@ -83,7 +71,7 @@ sudo nano /etc/init/mosquitto.conf
 description "Mosquitto MQTT broker"
 start on net-device-up
 respawn
-exec /usr/local/sbin/mosquitto -c /etc/mosquitto/mosquitto.conf
+exec /usr/local/sbin/mosquitto -c /etc/mosquitto/conf.d/default.conf
 
 // Run MQTT Broker on system start up
 sudo update-rc.d mosquitto remove
@@ -95,11 +83,13 @@ sudo nano /etc/systemd/system/mosquitto.service
 Description=Mosquitto MQTT Broker
 Documentation=man:mosquitto(8)
 Documentation=man:mosquitto.conf(5)
-ConditionPathExists=/etc/mosquitto/mosquitto.conf
+ConditionPathExists=/etc/mosquitto/conf.d/default.conf
+
 After=xdk-daemon.service
 
 [Service]
-ExecStart=/usr/local/sbin/mosquitto -c /etc/mosquitto/mosquitto.conf
+ExecStart=/usr/local/sbin/mosquitto -c /etc/mosquitto/conf.d/default.conf
+
 ExecReload=/bin/kill -HUP $MAINPID
 User=mosquitto
 Restart=on-failure
