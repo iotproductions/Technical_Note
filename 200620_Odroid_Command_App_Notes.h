031f23301b4a41e2dbe4593ef1e63843d6e13e06
IOT_Developer <lethanhtrieuk36@gmail.com>
2020-07-19 16:15:39 +0700

    Update 200620_Odroid_Command_App_Notes.h

--- 200620_Odroid_Command_App_Notes.h
+++ 200620_Odroid_Command_App_Notes.h
@@ -109,17 +109,7 @@ sudo apt-get install apache2
 //----------------------------------------------------------------------------------------------
 // Install PM2
 //----------------------------------------------------------------------------------------------
- # 1. Add the PM2 repository signing key
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv D1EA2D4C
-
-# 2. Add the PM2 repository
-echo "deb http://apt.pm2.io/ubuntu stable main" | sudo tee /etc/apt/sources.list.d/pm2.list
-
-# 3. Update list of available packages
-sudo apt-get update
-
-# 4. Install PM2
-sudo apt-get install pm2
+sudo npm install pm2 -g
 //----------------------------------------------------------------------------------------------
 // Install pm2-web-ui
 // PM2 Web monitoring Tool
